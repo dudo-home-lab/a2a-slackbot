c48c2cef20f71715ba02e621ae4d62c0660d70e6
Brett C. Dudo <brett@dudo.io>
2026-03-10 08:59:51 -0700

    try

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import app from './app.js';
 
 /** Start Bolt App */
-(async () => {
-  try {
-    await app.start(process.env.PORT || 3000);
-    app.logger.info('⚡️ Bolt app is running! ⚡️');
-  } catch (error) {
-    app.logger.error('Unable to start App', error);
-  }
-})();
+try {
+  await app.start(process.env.PORT || 3000);
+  app.logger.info('⚡️ Bolt app is running! ⚡️');
+} catch (error) {
+  app.logger.error('Unable to start App', error);
+}
